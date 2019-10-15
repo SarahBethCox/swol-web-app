1dc8dbae4419cd5233689535a2708e06e0370364
unknown <scox20@atu.edu>
2019-10-15 11:54:04 -0700

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
+import { OrderConfirmComponent } from './place-order/order-confirm/order-confirm/order-confirm.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'place-order',
+    component: PlaceOrderComponent
+  },
+  {
+    path: 'view-order',
+    component: ViewOrderComponent 
+  },
+  {
+    path: 'order-confirm',
+    component: OrderConfirmComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
+import { CoreModule } from './core/core.module';
+import { fromEvent } from 'rxjs';
+import { OrderConfirmComponent } from './place-order/order-confirm/order-confirm/order-confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    PlaceOrderComponent,
+    ViewOrderComponent,
+    OrderConfirmComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
