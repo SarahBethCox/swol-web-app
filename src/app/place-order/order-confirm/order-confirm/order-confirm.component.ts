a815397fa84a7281993662028ed1e54807a9f233
escleonh <hescalante@atu.edu>
2019-10-31 16:16:02 -0500

    fixed order confirm page

--- src/app/place-order/order-confirm/order-confirm/order-confirm.component.ts
+++ src/app/place-order/order-confirm/order-confirm/order-confirm.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { OrderService } from 'src/app/shared/order.service';
+
 
 @Component({
   selector: 'app-order-confirm',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderConfirmComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  orderDetails:any;
+  name:string;
+  from:string;
+  to:string
+  tier:number;
+  
+  constructor(private service:OrderService) {
+     
   }
 
+  ngOnInit() {
+   this.service.currentOrderInfo.subscribe(
+    (orderData)=>{
+      this.orderDetails = Object.assign({}, orderData);
+    }
+   )
+
+
+  
+  this.name = this.orderDetails.name;
+  this.from = this.orderDetails.from;
+  this.to = this.orderDetails.to;
+  this.tier = this.orderDetails.tierOption;
+    // console.log(this.orderDetails);
+  }
+  
+  
+
+  submitOrder(){
+   
+    
+  }
+  
 }
