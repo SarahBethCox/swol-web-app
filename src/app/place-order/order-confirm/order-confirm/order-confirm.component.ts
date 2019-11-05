f01c17b2e443c823bbf0836d5b3ba65e2f93c2d0
Sarah Cox <31324537+SarahBethCox@users.noreply.github.com>
2019-11-05 08:49:09 -0800

    Revert "merge order confirm branch"

--- src/app/place-order/order-confirm/order-confirm/order-confirm.component.ts
+++ src/app/place-order/order-confirm/order-confirm/order-confirm.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit} from '@angular/core';
-import { OrderService } from 'src/app/shared/order.service';
-import { AngularFirestore } from '@angular/fire/firestore';
-
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-order-confirm',
@@ -11,57 +7,9 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class OrderConfirmComponent implements OnInit {
 
-  orderDetails:any;
-  name:string;
-  from:string;
-  to:string
-  tier:number;
-  orderNumber:string;
-  randNum:number;
-
-  constructor(private service:OrderService,private firestore:AngularFirestore) {
-     
-  }
+  constructor() { }
 
   ngOnInit() {
-   this.service.currentOrderInfo.subscribe(
-    (orderData)=>{
-      this.orderDetails = Object.assign({}, orderData);
-    }
-   )
-
-
-  
-  this.name = this.orderDetails.name;
-  this.from = this.orderDetails.from;
-  this.to = this.orderDetails.to;
-  this.tier = this.orderDetails.tierOption;
-    // console.log(this.orderDetails);
   }
-  
-  createOrderNumber(){
-    this.orderNumber='';
-    
-    for(var i=0; i<10; i++){
-      this.randNum = Math.floor(Math.random() * 10) + 1;
-      this.orderNumber = this.orderNumber + this.randNum.toString();
-    }
-    this.orderNumber = this.name.substring(0,1) + this.orderNumber + this.name.substring(0,1);
-    
-    return this.orderNumber;
-  }
-  
-  
 
-  submitOrder(){
-   
-    this.orderDetails.orderNumber = this.createOrderNumber();
-
-    if(this.firestore.collection('orders').add(this.orderDetails)){
-      alert(this.orderDetails.name + " your order was stored");
-    }
-  
-  }
-  
-  
 }
